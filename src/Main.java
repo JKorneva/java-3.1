27521da4f5fb61b47602718157154eeb3ff72413
Julia Korneva <amalia9111@gmail.com>
2020-11-17 21:40:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int mil = 1;
+        float tiket = 21.50F;
+        float bonus = 20;
+        int res = (int) (tiket / bonus * mil);
+        System.out.println(res);
+
+    }
+}
